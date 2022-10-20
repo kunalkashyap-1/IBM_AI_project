613b1a2c4b698207a1fb93914259271fbda626b2
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-10-20 18:34:31 +0530

    replaced the repetative api calls with a function

--- views/assets/js/custom.js
+++ views/assets/js/custom.js
@@ -14,27 +14,11 @@ let data_mov1={
     "cast":document.querySelector(".cast1")
 }
 
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+data_mov1.movie_name, options)
-.then(response => response.json())
-.then((response) => {
-    data_mov1.image.setAttribute("src", response.d[0].i.imageUrl);
-    data_mov1.cast.innerHTML="Cast: "+response.d[0].s;
-})
-.catch(err => console.error(err));
-
 let data_mov2={
     "movie_name":document.querySelector(".mov2").textContent,
     "image":document.querySelector(".img2"),
     "cast":document.querySelector(".cast2")
 }
-
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+data_mov2.movie_name, options)
-.then(response => response.json())
-.then((response) => {
-    data_mov2.image.setAttribute("src", response.d[0].i.imageUrl);
-    data_mov2.cast.innerHTML="Cast: "+response.d[0].s;
-})
-.catch(err => console.error(err));
 
 let data_mov3={
     "movie_name":document.querySelector(".mov3").textContent,
@@ -42,29 +26,11 @@ let data_mov3={
     "cast":document.querySelector(".cast3")
 }
 
-
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+data_mov3.movie_name, options)
-.then(response => response.json())
-.then((response) => {
-    data_mov3.image.setAttribute("src", response.d[0].i.imageUrl);
-    data_mov3.cast.innerHTML="Cast: "+response.d[0].s;
-})
-.catch(err => console.error(err));
-
-
 let data_mov4={
     "movie_name":document.querySelector(".mov4").textContent,
     "image":document.querySelector(".img4"),
     "cast":document.querySelector(".cast4")
 }
-
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+data_mov4.movie_name, options)
-.then(response => response.json())
-.then((response) => {
-    data_mov4.image.setAttribute("src", response.d[0].i.imageUrl);
-    data_mov4.cast.innerHTML="Cast: "+response.d[0].s;
-})
-.catch(err => console.error(err));
 
 let data_mov5={
     "movie_name":document.querySelector(".mov5").textContent,
@@ -72,13 +38,23 @@ let data_mov5={
     "cast":document.querySelector(".cast5")
 }
 
-fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+data_mov5.movie_name, options)
-.then(response => response.json())
-.then((response) => {
-    data_mov5.image.setAttribute("src", response.d[0].i.imageUrl);
-    data_mov5.cast.innerHTML="Cast: "+response.d[0].s;
-})
-.catch(err => console.error(err));
+
+function getData(movie){
+    fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+movie.movie_name, options)
+    .then(response => response.json())
+    .then((response) => {
+        movie.image.setAttribute("src", response.d[0].i.imageUrl);
+        movie.cast.innerHTML="Cast: "+response.d[0].s;
+    })
+    .catch(err => console.error(err));
+    
+}
+
+getData(data_mov1);
+getData(data_mov2);
+getData(data_mov3);
+getData(data_mov4);
+getData(data_mov5);
 
 
 
